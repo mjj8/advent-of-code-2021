f0fdca598266c850ecba23a56a9498b8f7a2a47c
mjj <johnson.miles@gmail.com>
2021-12-06 17:10:28 -0500

    working haskell container and day01 tests

--- haskell/src/Day01.hs
+++ haskell/src/Day01.hs
@@ -1,16 +1,18 @@
+module Day01 where
 
 parta :: [Int] -> Int
 parta = count_greater
 
 partb :: [Int] -> Int
 partb xs = count_greater zs
-    where zs = [sum tup | tup <- triples xs]
+    where zs = [mysum tup | tup <- triples xs]
+          mysum (a, b, c) = a + b + c
     
 count_greater :: [Int] -> Int
 count_greater xs = length [(a, b) | (a, b) <- pairs xs, a < b]
 
 pairs :: [a] -> [(a, a)]
-paris xs = zip xs (tail xs)
+pairs xs = zip xs (tail xs)
 
 triples :: [a] -> [(a, a, a)]
 triples xs = zip3 xs (tail xs) (tail (tail xs))
--- haskell/tests/Spec.hs
+++ haskell/tests/Spec.hs
@@ -1,19 +1,32 @@
 module Main (main) where
+import Test.Hspec
 
-{-| someFunc 
->>> someFunc
--}
+import qualified Day01
 
 
-{-| triple 
->>> triple ""
-""
->>> triple "a"
-"aaa"
--}
+spec01 :: Spec
+spec01 = do
+    describe "day01 parta and partb" $ do
+        it "a simple test of parta" $ do
+            Day01.parta [0, 1, 0, 1] `shouldBe` 2
 
-triple :: [a] -> [a]
-triple l = l ++ l ++ l
+        it "the full parta" $ do
+            content <- readFile "src/input01.txt"
+            let xs = [read x:: Int | x <- lines content]
+            Day01.parta xs `shouldBe` 1288
+
+        it "a simple test of triples" $ do
+            Day01.triples [0, 1, 2, 3] `shouldBe` [(0,1,2), (1,2,3)]
+
+        it "the full partb" $ do
+            content <- readFile "src/input01.txt"
+            let xs = [read x:: Int | x <- lines content]
+            Day01.partb xs `shouldBe` 1311
+
+spec :: Spec
+spec = do
+    describe "day01" spec01
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = hspec spec
+-- main = putStrLn ("Test suite is not implemented" :: String)
